caa3fb47af2403911511cdcc4a95fca1d4d3eae5
idabblewith <jprincedev@gmail.com>
2025-02-03 14:48:18 +0800

    feat: accounts crud, data table, bulk delete
    - zustand, paralleldrive/cuid2, shadcn components, use-confirm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -30,7 +32,12 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				>
-					<QueryProvider>{children}</QueryProvider>
+					<QueryProvider>
+						{" "}
+						<SheetProvider />
+						<Toaster />
+						{children}
+					</QueryProvider>
 				</body>
 			</html>
 		</ClerkProvider>
